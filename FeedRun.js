baf6102a2197257176460dbaa0f4e2bb2f5ed5ed
sarfarazMohd <53967723+sarfarazMohd@users.noreply.github.com>
2022-10-20 22:47:05 +0530

    Create FeedRun.js

---
+++ FeedRun.js
@@ -0,0 +1,8 @@
+ <script type="text/javascript">
+      var feed = new Instafeed({
+        accessToken:
+          "IGQVJYWkZAIRklOdlRKWWU0SWFxOTYzLUdSdVVsVWt5TDY4Y25YQWVJOW1PczR0VWo4MmVlUVpLN01DUWJRTmVja2pCb2dtTUdhMmVwRmtYbGMyMmpyWGYxR1FRLUdDUkZAaRDhTWEl3M2VDTXRsVmlZAYW1zT2VTSGRFSkJ3",
+        template: '<img class="post" title="{{caption}}" src="{{image}}" />'
+      });
+      feed.run();
+    </script>
